33fb6c905c8d44b4ccd5a395fc4b101d218dd2f7
Shreya Veeramaneni <shreyaveeramaneni@Shreyas-Laptop.local>
2025-07-18 22:21:46 -0600

    Add Topological Sort implementation in Python

---
+++ topological_sort.py
@@ -0,0 +1,44 @@
+# topological_sort.py
+
+from collections import defaultdict
+
+class Graph:
+    def __init__(self, vertices):
+        self.graph = defaultdict(list)
+        self.V = vertices
+
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
+
+    def topological_sort_util(self, v, visited, stack):
+        visited[v] = True
+
+        for neighbor in self.graph[v]:
+            if not visited[neighbor]:
+                self.topological_sort_util(neighbor, visited, stack)
+
+        stack.append(v)
+
+    def topological_sort(self):
+        visited = [False] * self.V
+        stack = []
+
+        for i in range(self.V):
+            if not visited[i]:
+                self.topological_sort_util(i, visited, stack)
+
+        stack.reverse()
+        return stack
+
+if __name__ == "__main__":
+    g = Graph(6)
+    g.add_edge(5, 2)
+    g.add_edge(5, 0)
+    g.add_edge(4, 0)
+    g.add_edge(4, 1)
+    g.add_edge(2, 3)
+    g.add_edge(3, 1)
+
+    print("A possible Topological Sort of the given graph:")
+    order = g.topological_sort()
+    print(order)
